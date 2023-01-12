592afbaa0e04786ec42c39c68a8b5860b83d2ce0
Miquel Amorós <91978819+miquelamorosaldev@users.noreply.github.com>
2023-01-12 19:47:19 +0100

    Update prova.bat

--- m01-uf2/scripts/prova.bat
+++ m01-uf2/scripts/prova.bat
@@ -1,6 +1,7 @@
 @echo off
 
-echo "Abriendo webs"
+echo "Abriendo webs interesantes"
 start https://www.softone.es
-start https://www.softone.es
-start https://www.softone.es
+start https://www.google.com
+echo "Espero que te haya gustado. Dale al Like"
+PAUSE
